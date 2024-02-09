cb48382ca74b63e29e62d5e40df91f27b50216a2
Dima Tytenko <amgpetronass@gmail.com>
2024-02-09 13:08:05 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import data from '../data/data.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
 
-export const App = ({}) => {
+export const App = () => {
   return (
     <div className="app">
       <Profile
