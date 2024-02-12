d898859b4c746a1937b413c221c93b09e7ee3c50
Dima Tytenko <amgpetronass@gmail.com>
2024-02-12 10:32:34 +0200

    some fixes

--- src/components/Statistics/StatisticItem.jsx
+++ src/components/Statistics/StatisticItem.jsx
@@ -1,13 +1,6 @@
-export const StatisticItem = ({ id, label, percentage }) => {
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
+import { getRandomColor } from '../../helpers/getRandomColor';
 
+export const StatisticItem = ({ id, label, percentage }) => {
   const liStyle = {
     backgroundColor: getRandomColor(),
   };
